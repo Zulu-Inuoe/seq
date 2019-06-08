8dcc050eca71fec9aca9355dae3711261f025eb2
Wilfredo Velázquez-Rodríguez <zulu.inuoe@gmail.com>
2019-06-08 11:52:09 -0400

    Just read LICENSE.txt

--- src/enumerable/drivers/enumerable-stream.lisp
+++ src/enumerable/drivers/enumerable-stream.lisp
@@ -1,13 +1,3 @@
-;;;enumerable - enumerable implementation for CL, using cl-cont
-;;;Written in 2018 by Wilfredo Velázquez-Rodríguez <zulu.inuoe@gmail.com>
-;;;
-;;;To the extent possible under law, the author(s) have dedicated all copyright
-;;;and related and neighboring rights to this software to the public domain
-;;;worldwide. This software is distributed without any warranty.
-;;;You should have received a copy of the CC0 Public Domain Dedication along
-;;;with this software. If not, see
-;;;<http://creativecommons.org/publicdomain/zero/1.0/>.
-
 (in-package #:enumerable)
 
 (defmethod map-enumerable (fn (enumerable stream))
