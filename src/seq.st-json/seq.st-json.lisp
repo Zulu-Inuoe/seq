977d137cd56b64b0b8985240c08bb48f75c5e251
Wilfredo Velázquez-Rodríguez <zulu.inuoe@gmail.com>
2020-12-22 16:33:02 -0500

    "fix" st-json integration

--- src/seq.st-json/seq.st-json.lisp
+++ src/seq.st-json/seq.st-json.lisp
@@ -2,19 +2,9 @@
   (:use #:cl)
   (:import-from
    #:com.inuoe.seq
-   #:col-seq
-   #:lazy-seq))
+   #:col-seq))
 
 (in-package #:com.inuoe.seq.st-json)
 
 (defmethod col-seq ((obj st-json:jso))
-  (lazy-seq
-    (st-json:getjso*)
-    (let ((it (xpath:make-node-set-iterator node-set)))
-      (labels ((recurse ()
-                 (unless (xpath:node-set-iterator-end-p it)
-                   (cons (xpath:node-set-iterator-current it)
-                         (lazy-seq
-                           (xpath:node-set-iterator-next it)
-                           (recurse))))))
-        (recurse)))))
+  (st-json::jso-alist obj))
