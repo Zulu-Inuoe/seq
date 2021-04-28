0d29faf5b42e8ae15d73f76f57c1218afdd366f7
Wilfredo Velázquez-Rodríguez <zulu.inuoe@gmail.com>
2021-04-28 17:09:38 -0400

    Pass decls down since every expander needs it anyway

--- src/doseq/doseq.lisp
+++ src/doseq/doseq.lisp
@@ -54,75 +54,71 @@ and t2 is not a subtype of t1."
       (setf *%doseq-expanders* (stable-sort *%doseq-expanders* #'%subtype< :key #'car)))))
 
 (defmacro define-doseq-expander
-    (type (iter-whole iter-type iter-var iter-i iter-enum iter-res iter-body iter-env)
+    (type (iter-whole iter-type iter-var iter-i iter-enum iter-res iter-decls iter-body iter-env)
      &body body)
   `(eval-when (:compile-toplevel :load-toplevel :execute)
      (%set-expander
       ',type
-      (lambda (,iter-whole ,iter-type ,iter-var ,iter-i ,iter-enum ,iter-res ,iter-body ,iter-env)
-        (declare (ignorable ,iter-whole ,iter-type ,iter-var ,iter-enum ,iter-res ,iter-body ,iter-env))
+      (lambda (,iter-whole ,iter-type ,iter-var ,iter-i ,iter-enum ,iter-res ,iter-decls ,iter-body ,iter-env)
+        (declare (ignorable ,iter-whole ,iter-type ,iter-var ,iter-enum ,iter-res ,iter-decls ,iter-body ,iter-env))
         ,@body))
      ',type))
 
 ;;; Base expanders
 
 (define-doseq-expander vector
-    (whole type var i col result body env)
+    (whole type var i col result decls body env)
   (let ((vec (gensym "VEC"))
         (idx (or i (gensym "I"))))
-    (multiple-value-bind (body decls) (parse-body body :whole whole)
-      `(let ((,vec ,col))
-         (dotimes (,idx (length ,vec) ,@(when result `((let (,var ,@(when i `(,i))) ,var ,@(when i `(,i)) ,result))))
-           (let ((,var (aref ,vec ,idx))
-                 ,@(when i `((,i ,idx))))
-             ,@decls
-             (tagbody ,@body)))))))
+    `(let ((,vec ,col))
+       (dotimes (,idx (length ,vec) ,@(when result `((let (,var ,@(when i `(,i))) ,var ,@(when i `(,i)) ,result))))
+         (let ((,var (aref ,vec ,idx))
+               ,@(when i `((,i ,idx))))
+           ,@decls
+           (tagbody ,@body))))))
 
 (define-doseq-expander hash-table
-    (whole type var i col result body env)
+    (whole type var i col result decls body env)
   (with-gensyms (iter more? key value)
-    (multiple-value-bind (body decls) (parse-body body :whole whole)
-      `(with-hash-table-iterator (,iter ,col)
-         (do ,(when i `((,i 0 (1+ ,i))))
-             (nil)
-           (multiple-value-bind (,more? ,key ,value)
-               (,iter)
-             (unless ,more?
-               (return ,@(when result `((let (,var ,@(when i `(,i))) ,var ,@(when i `(,i)) ,result)))))
-             (let ((,var (cons ,key ,value))
-                   ,@(when i `((,i ,i))))
-               ,@decls
-               (tagbody ,@body))))))))
-
-(define-doseq-expander stream
-    (whole type var i col result body env)
-  (with-gensyms (stream-sym type-sym reader-sym elt-sym)
-    (multiple-value-bind (body decls) (parse-body body :whole whole)
-      `(let* ((,stream-sym ,col)
-              (,type-sym (stream-element-type ,stream-sym))
-              (,reader-sym (cond
-                             ((subtypep ,type-sym 'integer) #'read-byte)
-                             ((subtypep ,type-sym 'characater) #'read-char)
-                             (t (error "unsupported stream type '~A'" ,type-sym)))))
-         (do ((,elt-sym (funcall ,reader-sym ,stream-sym nil) (funcall ,reader-sym ,stream-sym nil))
-              ,@(when i `((,i 0 (1+ i)))))
-             ((null ,elt-sym) ,@(when result `((let (,var ,@(when i `(,i))) ,var ,@(when i `(,i)) ,result))))
-           (let ((,var ,elt-sym)
+    `(with-hash-table-iterator (,iter ,col)
+       (do ,(when i `((,i 0 (1+ ,i))))
+           (nil)
+         (multiple-value-bind (,more? ,key ,value)
+             (,iter)
+           (unless ,more?
+             (return ,@(when result `((let (,var ,@(when i `(,i))) ,var ,@(when i `(,i)) ,result)))))
+           (let ((,var (cons ,key ,value))
                  ,@(when i `((,i ,i))))
              ,@decls
              (tagbody ,@body)))))))
 
-(define-doseq-expander lazy-seq
-    (whole type var i col result body env)
-  (with-gensyms (seq-sym)
-    (multiple-value-bind (body decls) (parse-body body :whole whole)
-      `(do ((,seq-sym (col-seq ,col) (col-seq (seq-rest ,seq-sym)))
+(define-doseq-expander stream
+    (whole type var i col result decls body env)
+  (with-gensyms (stream-sym type-sym reader-sym elt-sym)
+    `(let* ((,stream-sym ,col)
+            (,type-sym (stream-element-type ,stream-sym))
+            (,reader-sym (cond
+                           ((subtypep ,type-sym 'integer) #'read-byte)
+                           ((subtypep ,type-sym 'characater) #'read-char)
+                           (t (error "unsupported stream type '~A'" ,type-sym)))))
+       (do ((,elt-sym (funcall ,reader-sym ,stream-sym nil) (funcall ,reader-sym ,stream-sym nil))
             ,@(when i `((,i 0 (1+ i)))))
-           ((null ,seq-sym) ,@(when result `((let (,var ,@(when i `(,i))) ,var ,@(when i `(,i)) ,result))))
-         (let ((,var (seq-first ,seq-sym))
+           ((null ,elt-sym) ,@(when result `((let (,var ,@(when i `(,i))) ,var ,@(when i `(,i)) ,result))))
+         (let ((,var ,elt-sym)
                ,@(when i `((,i ,i))))
            ,@decls
            (tagbody ,@body))))))
+
+(define-doseq-expander lazy-seq
+    (whole type var i col result decls body env)
+  (with-gensyms (seq-sym)
+    `(do ((,seq-sym (col-seq ,col) (col-seq (seq-rest ,seq-sym)))
+          ,@(when i `((,i 0 (1+ i)))))
+         ((null ,seq-sym) ,@(when result `((let (,var ,@(when i `(,i))) ,var ,@(when i `(,i)) ,result))))
+       (let ((,var (seq-first ,seq-sym))
+             ,@(when i `((,i ,i))))
+         ,@decls
+         (tagbody ,@body)))))
 
 ;;; Type derivation
 
@@ -212,19 +208,18 @@ and t2 is not a subtype of t1."
               ((null (cdr var)) (values (first var) nil))
               ((null (cddr var)) (values (first var) (second var)) )
               (t (error "Invalid doseq binding '~A'" var))))
-        (funcall expander whole exp-type var i col result body env)))))
+        (multiple-value-bind (body decls) (parse-body body :whole whole)
+          (funcall expander whole exp-type var i col result decls body env))))))
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
-  (defun %map-expander (whole type var i col result body env)
-    (declare (ignore type env))
-    (multiple-value-bind (body decl)
-        (parse-body body :whole whole)
-      `(block nil
-         ,@(if i
-               `((mapcol* ,col (lambda (,var ,i) ,@decl (tagbody ,@body)))
-                 ,(when result `(let (,var ,i) ,var ,i ,result)))
-               `((mapcol ,col (lambda (,var) ,@decl (tagbody ,@body)))
-                 ,(when result `(let (,var) ,var ,result))))))))
+  (defun %map-expander (whole type var i col result decls body env)
+    (declare (ignore whole type env))
+    `(block nil
+       ,@(if i
+             `((mapcol* ,col (lambda (,var ,i) ,@decls (tagbody ,@body)))
+               ,(when result `(let (,var ,i) ,var ,i ,result)))
+             `((mapcol ,col (lambda (,var) ,@decls (tagbody ,@body)))
+               ,(when result `(let (,var) ,var ,result)))))))
 
 (defmacro doseq (&whole whole (var col &optional result)
                  &body body
